3145c4edecef59dff8ac0024e984cf765bcb8eba
mhx <mhxsoul521@163.com>
2021-08-18 13:44:58 +0800

    third commit

--- src/main/java/com/mhx/git/GitTest.java
+++ src/main/java/com/mhx/git/GitTest.java
@@ -3,5 +3,6 @@ package com.mhx.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git version2!");
+        System.out.println("hello git version3!");
     }
 }

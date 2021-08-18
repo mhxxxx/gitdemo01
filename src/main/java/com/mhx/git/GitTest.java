5eb081588c406bc17e59ce5030daa41bfdf75137
mhx <mhxsoul521@163.com>
2021-08-18 13:51:54 +0800

    hot-fix commit1

--- src/main/java/com/mhx/git/GitTest.java
+++ src/main/java/com/mhx/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git version2!");
         System.out.println("hello git version3!");
+        System.out.println("hello git hot-fix version!");
     }
 }
